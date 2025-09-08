c570ba7e4ae35ab16fef7d65e8c0f5c8578c2fdc
Leonardo <leonardomullerdelima@pm.me>
2025-09-08 09:40:31 -0300

    feat: add basic operation functions

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+static float Add(float n1, float n2)
+{
+    return n1 + n2;
+}
+
+static float Subtract(float n1, float n2)
+{
+    return n1 - n2;
+}
+
+static float Divide(float n1, float n2)
+{
+    return n1 / n2;
+}
+
+static float Multiply(float n1, float n2)
+{
+    return n1 * n2;
+}
