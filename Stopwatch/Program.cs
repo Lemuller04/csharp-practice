70aa7791c21458f8d98ac312c8b9fbb13d02c234
Leonardo <leonardomullerdelima@pm.me>
2025-09-08 11:03:42 -0300

    feat: use loop instead of recursion

--- Stopwatch/Program.cs
+++ Stopwatch/Program.cs
@@ -1,18 +1,21 @@
 ﻿static void Menu()
 {
-    Console.Clear();
-    Console.WriteLine("[x]s para contar x segundos.\n[x]m para contar x minutos.\n'q' para sair.");
+    while (true)
+    {
+        Console.Clear();
+        Console.WriteLine("[x]s para contar x segundos.\n[x]m para contar x minutos.\n'q' para sair.");
 
-    string data = Console.ReadLine().ToLower();
+        string data = Console.ReadLine().ToLower();
 
-    if (data == "q") Environment.Exit(0);
+        if (data == "q") Environment.Exit(0);
 
-    char type = char.Parse(data.Substring(data.Length - 1, 1));
-    int time = int.Parse(data.Substring(0, data.Length - 1));
-    int multiplier = 1;
+        char type = char.Parse(data.Substring(data.Length - 1, 1));
+        int time = int.Parse(data.Substring(0, data.Length - 1));
+        int multiplier = 1;
 
-    if (type == 'm') multiplier = 60;
-    Start(time * multiplier);
+        if (type == 'm') multiplier = 60;
+        Start(time * multiplier);
+    }
 }
 
 static void Start(int time)
@@ -31,7 +34,6 @@ static void Start(int time)
     Console.WriteLine(currentTime);
     Console.WriteLine("Stopwatch finalizado");
     Console.ReadKey();
-    Menu();
 }
 
 Menu();
