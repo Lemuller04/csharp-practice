9c8cac3b6bbd0a8798d8dd62ad9d48e570097ce0
Leonardo <leonardomullerdelima@pm.me>
2025-09-08 10:00:42 -0300

    feat: add input validation

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -4,15 +4,16 @@
     {
         Console.Clear();
         Console.WriteLine("Executar:\n(1) Adição\n(2) Subtração\n(3) Divisão\n(4) Multiplicação\n(5) Sair");
-        short operation = short.Parse(Console.ReadLine());
+
+        float operation = GetFloat();
 
         if (operation == 5) Environment.Exit(0);
         if (operation < 1 || operation > 5) continue;
 
         Console.Write("N1: ");
-        float x1 = float.Parse(Console.ReadLine());
+        float x1 = GetFloat();
         Console.Write("N2: ");
-        float x2 = float.Parse(Console.ReadLine());
+        float x2 = GetFloat();
 
         float res = operation switch
         {
@@ -26,6 +27,18 @@
         Console.WriteLine($"\nO resultado é {res}");
         Console.ReadKey();
     }
+}
+
+static float GetFloat()
+{
+    while (true)
+    {
+        if (float.TryParse(Console.ReadLine(), out float entry))
+        {
+            return entry;
+        }
+    }
+
 }
 
 static float Add(float n1, float n2)
