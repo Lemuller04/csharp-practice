da2599c8bb53fb5d541557fbf1ad3de09bc5d2fc
Leonardo <leonardomullerdelima@pm.me>
2025-09-08 09:47:29 -0300

    feat: replace recursion with infinte loop and use better sintaxe

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,38 +1,31 @@
 ﻿static void Menu()
 {
-    Console.Clear();
-
-    Console.WriteLine("Executar:\n(1) Adição\n(2) Subtração\n(3) Divisão\n(4) Multiplicação\n(5) Sair");
-    short operation = short.Parse(Console.ReadLine());
-
-    switch (operation)
+    while (true)
     {
-        case 1: break;
-        case 2: break;
-        case 3: break;
-        case 4: break;
-        case 5: System.Environment.Exit(0); break;
-        default: Menu(); break;
+        Console.Clear();
+        Console.WriteLine("Executar:\n(1) Adição\n(2) Subtração\n(3) Divisão\n(4) Multiplicação\n(5) Sair");
+        short operation = short.Parse(Console.ReadLine());
+
+        if (operation == 5) Environment.Exit(0);
+        if (operation < 1 || operation > 5) continue;
+
+        Console.Write("N1: ");
+        float x1 = float.Parse(Console.ReadLine());
+        Console.Write("N2: ");
+        float x2 = float.Parse(Console.ReadLine());
+
+        float res = operation switch
+        {
+            1 => Add(x1, x2),
+            2 => Subtract(x1, x2),
+            3 => Divide(x1, x2),
+            4 => Multiply(x1, x2),
+            _ => 0f
+        };
+
+        Console.WriteLine($"\nO resultado é {res}");
+        Console.ReadKey();
     }
-
-    Console.Write("N1: ");
-    float x1 = float.Parse(Console.ReadLine());
-    Console.Write("N2: ");
-    float x2 = float.Parse(Console.ReadLine());
-
-    float res = 0f;
-
-    switch (operation)
-    {
-        case 1: res = Add(x1, x2); break;
-        case 2: res = Subtract(x1, x2); break;
-        case 3: res = Divide(x1, x2); break;
-        case 4: res = Multiply(x1, x2); break;
-    }
-
-    Console.WriteLine($"\nO resultado é {res}");
-    Console.ReadKey();
-    Menu();
 }
 
 static float Add(float n1, float n2)
