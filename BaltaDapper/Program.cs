ed86ed691499bc21cf405361fb6ed74c2b75f0bf
Leonardo <leonardomullerdelima@pm.me>
2025-09-17 13:43:49 -0300

    feat: Dapper practice setup

---
+++ BaltaDapper/Program.cs
@@ -0,0 +1,15 @@
+﻿using DotNetEnv;
+using Dapper;
+
+namespace BaltaDapper
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Env.TraversePath().Load();
+            var dbHost = Environment.GetEnvironmentVariable("DB_HOST");
+            Console.WriteLine(dbHost);
+        }
+    }
+}
